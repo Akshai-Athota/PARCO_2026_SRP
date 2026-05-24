a2ff03d44b5d0571b50c38561f53f0df67b7ca54
athota <athota@master.ismll.de>
2026-05-24 08:16:30 +0200

    hcrvp problem completed

--- run_test_omdcpdp_greedy.sh
+++ run_test_omdcpdp_greedy.sh
@@ -5,9 +5,11 @@
 #SBATCH --mail-user=athota@uni-hildesheim.de
 #SBATCH --mail-type=ALL
 #SBATCH --partition=STUD
-#SBATCH --gres=gpu:2       
+#SBATCH --gres=gpu:2
+#SBATCH --mem=32G
 
 source .venv/bin/activate
+
 export WANDB_MODE=offline
 export WANDB_DIR=$SLURM_SUBMIT_DIR/wandb
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
@@ -16,5 +18,4 @@ srun python test.py \
   --problem omdcpdp \
   --checkpoint logs/train/runs/omdcpdp_n100_m50/parco/2026-05-18_14-40-06/checkpoints/epoch_095.ckpt \
   --decode_type greedy \
-  --batch_size 2 \           
-  --no_aug       
+  --batch_size 2       
